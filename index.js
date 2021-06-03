a8a7dcb2230366fbee590fa6c2140dbd5b2794ad
Cooper Jackson <davisjaxon@gmail.com>
2021-06-03 14:50:17 -0700

    adding heroku access

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const server = require('./api/server');
+require('dotenv').config(); // add this line as the first thing to run1
 
-// START YOUR SERVER HERE
-server.listen(9000, () => {
-    console.log('listening on 9000')
-})
+const server = require('./api/server.js');
+
+// we'll read the port from the server environment if it is there
+// Heroku will have the PORT environment variable set
+const port = process.env.PORT || 5000;
+
+// we can now use that port, if set up by heroku or read from .env or 5000 as a default if not set
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
